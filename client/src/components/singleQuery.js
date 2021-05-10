511d69c8c920855eb61cdaf5e5fedd9815f50bea
practice404 <hawkempire007@gmail.com>
2021-05-10 16:45:43 +0530

    small bug

--- client/src/components/singleQuery.js
+++ client/src/components/singleQuery.js
@@ -43,7 +43,7 @@ function SingleQuery({ feed = {}, getQuery, currentUser, loading }) {
             <p>
               {!currentUser && (
                 <Alert variant="danger">
-                  You need to login or register before adding your reply
+                  You need to login or register before adding comment
                 </Alert>
               )}
             </p>
